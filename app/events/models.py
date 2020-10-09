50155b40794862420be64b76c6986fa5918838b0
PauloJBrasil <paulojosafa@hotmail.com>
2020-10-09 03:35:05 -0300

    Atualizando Models e adicionando views

---
+++ app/events/models.py
@@ -0,0 +1,32 @@
+from datetime import timedelta
+
+from django.db import models
+
+# Create your models here.
+from django.utils import timezone
+
+
+class Event(models.Model):
+    title = models.TextField(null=False)
+    description = models.TextField(null=False)
+    category = models.CharField(max_length=250)
+    pub_date = models.DateTimeField('date published')
+    date = models.DateTimeField(null=False)
+    limit_ticket = models.IntegerField(default=0, editable=True)
+
+    def __str__(self):
+        return self.title
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - timedelta(days=1)
+
+class Ticket(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    code = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.code
+
+class UserProfile(models.Model):
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
